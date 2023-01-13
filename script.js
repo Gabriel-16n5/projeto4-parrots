dc8c957e41a81da2f88d89546b13772d37339749
gabriel rodrigues <gabrielgranjeia@alunos.utfpr.edu.br>
2023-01-13 18:06:34 -0300

    Primeira versão do randomizador das cartas na tela*teste

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let qtdCard = prompt("Quantas cartas quer jogar?(de 4 a 14 APENAS)");
 let verificador = 1;
 let cont = 0;
+const baralhoPronto = [];
+const baralho = document.querySelectorAll('.card.tres, .card.um, .card.quatro, .card.dois, .card.sete, .card.cinco, .card.seis');
+const baralho2 = document.querySelectorAll('.card.um');
+
 
 while(verificador != 0){
     if((qtdCard < 4) || (qtdCard >14) || (qtdCard%2!=0)){
@@ -12,14 +16,25 @@ while(verificador != 0){
     }
 }
 
-//const baralho = [];
-const baralho = document.querySelectorAll('.card');
+
+function nodetoarr(){
+    for(index = 0 ; index < 14 ; index++){
+        baralhoPronto[index] = baralho[index];
+    }
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
 function puzzle(){
     for(let i = 0 ; i < qtdCard; i++){
         //let baralhoEmbaralhado = parseInt(Math.random() * qtdCard);
-        baralho[cont].classList.remove('escondido');
-        cont++;
+        baralhoPronto[i].classList.remove('escondido');
+        //cont++;
     }
 }
+
+nodetoarr();
+baralhoPronto.sort(comparador);
 puzzle();
