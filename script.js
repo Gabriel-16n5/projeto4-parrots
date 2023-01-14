eb0704e078d53a077e86fc83000c6e097e0021c6
gabriel rodrigues <gabrielgranjeia@alunos.utfpr.edu.br>
2023-01-14 19:10:26 -0300

    Terminado os requisitos Clique na Carta

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const baralho = document.querySelectorAll('.card.tres, .card.um, .card.quatro, .
 const baralho1 = document.querySelectorAll('.card.um');
 const baralho2 = document.querySelectorAll('.card.dois');
 const baralhoo = baralho1 + baralho2;
-
+let qualcarta;
+let verifi = 0;
+let ultimaCarta1 = [];
+let ultimaCarta2 = [];
+let armaze = 0;
+let oi = 0;
 
 while(verificador != 0){
     if((qtdCard < 4) || (qtdCard >14) || (qtdCard%2!=0)){
@@ -37,16 +42,58 @@ function puzzle(){
     }
 }
 
-function viraCard(recebe){
-    const cartaViradaGif = recebe.querySelector('.escondido');
-    console.log(cartaViradaGif);
-    cartaViradaGif.classList.remove('escondido');
-    console.log(recebe);
-    const cartaVirada = recebe.querySelector('.atras');
-    console.log(cartaVirada);
-    cartaVirada.classList.add('escondido');
-    
+function viraCard(recebe, recebeClass){
+    if(qualcarta === recebeClass){
+        verifi = 0;
+        alert('MermaCarta')
+        //qualcarta = recebeClass;
+        const cartaViradaGif = recebe.querySelector('.escondido');
+        console.log(cartaViradaGif);
+        cartaViradaGif.classList.remove('escondido');
+        console.log(recebe);
+        const cartaVirada = recebe.querySelector('.atras');
+        console.log(cartaVirada);
+        cartaVirada.classList.add('escondido');
+        oi++;
+        armaze++;
+    } else{
+        qualcarta = recebeClass;
+        ultimaCarta1[armaze] = recebe.querySelector('.escondido');
+        ultimaCarta2[armaze] = recebe.querySelector('.atras');
+        const cartaViradaGif = recebe.querySelector('.escondido');
+        console.log(cartaViradaGif);
+        cartaViradaGif.classList.remove('escondido');
+        console.log(recebe);
+        const cartaVirada = recebe.querySelector('.atras');
+        console.log(cartaVirada);
+        cartaVirada.classList.add('escondido');
+        verifi++;
+        armaze++;
+        oi++;
+    }
+    if(verifi == 2){
+        setTimeout(Espera, 1000);
+    }
 }
+
+function erou(){
+    alert('Errooou');
+}
+
+
+function Espera(){   
+    verifi = 0;
+    oi = oi - 2;
+    ultimaCarta1[oi].classList.add('escondido');
+    ultimaCarta2[oi].classList.remove('escondido');
+    oi++;
+    ultimaCarta1[oi].classList.add('escondido');
+    ultimaCarta2[oi].classList.remove('escondido');
+    oi++;
+    qualcarta = '';
+}
+
+
 
 nodetoarr();
 baralhoPronto.sort(comparador);
